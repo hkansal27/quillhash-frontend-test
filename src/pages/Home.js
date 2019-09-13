7afca8802bf1654602e3e5d8cd2e47f05708ba45
Himanshu  Kansal <himanshukansal.jiit@gmail.com>
2019-09-13 20:03:10 +0530

    added unslash api, ImageCard, ImageContainer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default function Home() {
-    return (
-        <div>
-            hello from home
-        </div>
-    )
+import ImageContainer from '../components/ImageContainer';
+
+import unsplash from "../api/unsplash";
+
+export default class Home extends Component {
+    state = { images: [] };
+    constructor(props) {
+        super(props);
+        this.getImages = this.getImages.bind(this);
+        this.getImages('a');
+    }
+
+    getImages = async (term) => {
+        const response = await unsplash.get("/search/photos", {
+            params: {
+                query: term
+            }
+        });
+        this.setState({ images: response.data.results });
+        console.log(response);
+    };
+    render() {
+        return (
+            <section className="ui container" style={{ marginTop: "10px" }}>
+                <ImageContainer images={this.state.images} />
+            </section>
+        );
+    }
 }
